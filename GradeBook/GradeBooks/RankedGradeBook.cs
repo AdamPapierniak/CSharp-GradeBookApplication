14b5f4b439243da3f06ab61a44882bbb59efd451
AdamPapierniak <Adam.Papierniak@gmail.com>
2021-01-23 00:36:10 -0600

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,59 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numberOfStudents = Students.Count;
+            List<double> ListOfGrades = new List<double>();
+
+            if (numberOfStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (Student student in Students)
+            {
+                foreach (double grade in student.Grades)
+                    ListOfGrades.Add(grade);
+            }
+
+            ListOfGrades.Sort((x,y)=> y.CompareTo(x));
+
+            if (averageGrade >= ListOfGrades[GetGradeCutoff(20)])
+            {
+                return 'A';
+            }
+            else if (averageGrade >= ListOfGrades[GetGradeCutoff(40)])
+            {
+                return 'B';
+            }
+            else if (averageGrade >= ListOfGrades[GetGradeCutoff(60)])
+            {
+                return 'C';
+            }
+            else if (averageGrade >= ListOfGrades[GetGradeCutoff(80)])
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
+
+        }
+
+        public int GetGradeCutoff(int percentage)
+        {
+            double numberOfGrades = 0;
+
+            foreach (Student student in Students)
+            {
+                numberOfGrades += student.Grades.Count;
+            }
+                     
+            return (int)Math.Round(numberOfGrades * percentage / 100.0, MidpointRounding.ToEven) - 1;
+        }
     }
 }
