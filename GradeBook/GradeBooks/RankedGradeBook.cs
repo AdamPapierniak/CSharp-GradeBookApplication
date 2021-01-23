ffdfab49713edc8bfcd03e86965e490f88b6fe49
AdamPapierniak <Adam.Papierniak@gmail.com>
2021-01-23 14:13:26 -0600

    Override Statistics

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -65,5 +65,29 @@ namespace GradeBook.GradeBooks
                      
             return (int)Math.Round(numberOfGrades * percentage / 100.0, MidpointRounding.ToEven) - 1;
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+         }
     }
 }
